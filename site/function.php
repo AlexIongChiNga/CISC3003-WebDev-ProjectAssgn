7d565cbadc84b7b5ecae7c508eb759e3bedc436e
Michael-W-o-n-g <dc22785@um.edu.mo>
2025-04-21 19:00:00 +0800

    last

--- site/function.php
+++ site/function.php
@@ -17,3 +17,23 @@ function check_login($con)
     header("Location: login.php");
     exit;
 }
+
+function random_num($length)
+{
+
+	$text = "";
+	if($length < 5)
+	{
+		$length = 5;
+	}
+
+	$len = rand(4,$length);
+
+	for ($i=0; $i < $len; $i++) { 
+		# code...
+
+		$text .= rand(0,9);
+	}
+
+	return $text;
+}
